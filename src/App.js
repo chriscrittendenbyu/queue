ea9764a02faaffeb47e72fe649e078a7ab186a71
Chris Crittenden <chris.crittenden@gmail.com>
2020-03-18 11:40:09 -0600

    Final Project

--- src/App.js
+++ src/App.js
@@ -1,177 +1,50 @@
 import React, { useState, useEffect } from "react";
 import MenuIcon from "@material-ui/icons/Menu";
+import Delete from "@material-ui/icons/Delete";
 import {
   AppBar,
   Button,
+  Checkbox,
   Drawer,
   IconButton,
   List,
   ListItem,
   ListItemText,
   Paper,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
   TextField,
   Toolbar,
   Typography
 } from "@material-ui/core";
 import { Link, Route } from "react-router-dom";
-import { auth } from "./firebase";
-
-export function SignIn(props) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(u => {
-      if (u) {
-        props.history.push("/app");
-      }
-      // do something
-    });
-
-    return unsubscribe;
-  }, [props.history]);
-
-  const handleSignIn = () => {
-    auth
-      .signInWithEmailAndPassword(email, password)
-      .then(() => {})
-      .catch(error => {
-        alert(error.message);
-      });
-  };
-
-  return (
-    <div>
-      <AppBar position="static" color="primary">
-        <Toolbar>
-          <Typography variant="h6" color="inherit">
-            Sign In
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <Paper style={{ width: "480px", marginTop: "50px", padding: "30px" }}>
-          <TextField
-            placeholder={"Email"}
-            fullWidth={true}
-            value={email}
-            onChange={e => {
-              setEmail(e.target.value);
-            }}
-          />
-          <TextField
-            type={"password"}
-            placeholder="Password"
-            fullWidth={true}
-            style={{ marginTop: "30px" }}
-            value={password}
-            onChange={e => {
-              setPassword(e.target.value);
-            }}
-          />
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-              marginTop: "30px"
-            }}
-          >
-            <Typography>
-              Don't have an account? <Link to="/signup">Sign up!</Link>
-            </Typography>
-            <Button color="primary" variant="contained" onClick={handleSignIn}>
-              Sign in
-            </Button>
-          </div>
-        </Paper>
-      </div>
-    </div>
-  );
-}
-
-export function SignUp(props) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(u => {
-      if (u) {
-        props.history.push("/app");
-      }
-      // do something
-    });
-
-    return unsubscribe;
-  }, [props.history]);
-
-  const handleSignUp = () => {
-    auth
-      .createUserWithEmailAndPassword(email, password)
-      .then(() => {})
-      .catch(error => {
-        alert(error.message);
-      });
-  };
-
-  return (
-    <div>
-      <AppBar position="static" color="primary">
-        <Toolbar>
-          <Typography variant="h6" color="inherit">
-            Sign Up
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <Paper style={{ width: "480px", marginTop: "50px", padding: "30px" }}>
-          <TextField
-            placeholder={"Email"}
-            fullWidth={true}
-            value={email}
-            onChange={e => {
-              setEmail(e.target.value);
-            }}
-          />
-          <TextField
-            type={"password"}
-            placeholder="Password"
-            fullWidth={true}
-            style={{ marginTop: "30px" }}
-            value={password}
-            onChange={e => {
-              setPassword(e.target.value);
-            }}
-          />
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-              marginTop: "30px"
-            }}
-          >
-            <Typography>
-              Already have an account? <Link to="/">Sign in!</Link>
-            </Typography>
-            <Button color="primary" variant="contained" onClick={handleSignUp}>
-              Sign up
-            </Button>
-          </div>
-        </Paper>
-      </div>
-    </div>
-  );
-}
+import { auth, db } from "./firebase";
 
 export function App(props) {
-  const [drawer_open, setDrawerOpen] = useState(false);
   const [user, setUser] = useState(null);
+  const [queue, setQueue] = useState([]);
+  const [newQueue, setNewQueue] = useState("");
+  const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(u => {
       if (u) {
         setUser(u);
+        console.log(u.uid);
+        db.collection("users")
+          .doc(u.uid)
+          .get()
+          .then(s => {
+            console.log(s.data());
+            if (s.data().admin) {
+              setIsAdmin(true);
+            } else {
+              setIsAdmin(false);
+            }
+          });
       } else {
         props.history.push("/");
       }
@@ -179,6 +52,20 @@ export function App(props) {
 
     return unsubscribe;
   }, [props.history]);
+
+  useEffect(() => {
+    db.collection("queue")
+      .orderBy("time")
+      .onSnapshot(snapshot => {
+        const updatedQueue = snapshot.docs.map(d => {
+          const data = d.data();
+          data.id = d.id;
+          return data;
+        });
+        console.log(updatedQueue);
+        setQueue(updatedQueue);
+      });
+  }, []);
 
   const handleSignOut = () => {
     auth
@@ -191,6 +78,20 @@ export function App(props) {
       });
   };
 
+  const addQueue = () => {
+    db.collection("queue")
+      .add({ time: new Date(), name: newQueue, user_id: user.uid })
+      .then(r => {
+        setNewQueue("");
+      });
+  };
+
+  const deleteQueue = id => {
+    db.collection("queue")
+      .doc(id)
+      .delete();
+  };
+
   if (!user) {
     return <div />;
   }
@@ -199,14 +100,6 @@ export function App(props) {
     <div>
       <AppBar position="static" color="primary">
         <Toolbar>
-          <IconButton
-            color="inherit"
-            onClick={() => {
-              setDrawerOpen(true);
-            }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography
             variant="h6"
             color="inherit"
@@ -222,18 +115,62 @@ export function App(props) {
           </Button>
         </Toolbar>
       </AppBar>
-      <Drawer
-        open={drawer_open}
-        onClose={() => {
-          setDrawerOpen(false);
-        }}
+      <div
+        style={{ display: "flex", justifyContent: "center", paddingTop: 30 }}
       >
-        <List>
-          <ListItem button>
-            <ListItemText primary="Home" />
-          </ListItem>
-        </List>
-      </Drawer>
+        <Paper style={{ padding: 20, maxWidth: 500, width: "100%" }}>
+          <div
+            style={{ display: "flex", alignItems: "center", marginBottom: 40 }}
+          >
+            <TextField
+              fullWidth
+              label="Sign Up for the Queue"
+              value={newQueue}
+              onChange={e => {
+                setNewQueue(e.target.value);
+              }}
+            />
+            <div>
+              <Button
+                variant="contained"
+                color="primary"
+                style={{ marginLeft: 15 }}
+                onClick={addQueue}
+              >
+                ADD
+              </Button>
+            </div>
+          </div>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Done</TableCell>
+                <TableCell>Order</TableCell>
+                <TableCell>Name</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {queue.map((q, i) => {
+                return (
+                  <TableRow key={q.id}>
+                    <TableCell size="small">
+                      <Checkbox
+                        checked={false}
+                        onChange={() => {
+                          deleteQueue(q.id);
+                        }}
+                        disabled={!(user.uid === q.user_id || isAdmin)}
+                      />
+                    </TableCell>
+                    <TableCell size="small">{i + 1}</TableCell>
+                    <TableCell size="small">{q.name}</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </Paper>
+      </div>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyCPBwcpGEWH9Rkkzqad2o5awZlOs6xQEBY",
+  authDomain: "amazing-project-2d55c.firebaseapp.com",
+  databaseURL: "https://amazing-project-2d55c.firebaseio.com",
+  projectId: "amazing-project-2d55c",
+  storageBucket: "amazing-project-2d55c.appspot.com",
+  messagingSenderId: "237748113772",
+  appId: "1:237748113772:web:5f82dd110145c9cd256349"
 };
 
 firebase.initializeApp(config);
